08d9f4c233b5418e0254ff357197fb11aa7b22fe
Salym Senyonga <salymsash@gmail.com>
2019-05-09 18:57:25 +0300

    Create preliminary landing page UI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("popper.js")
+require("bootstrap")
+require("mdb")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
